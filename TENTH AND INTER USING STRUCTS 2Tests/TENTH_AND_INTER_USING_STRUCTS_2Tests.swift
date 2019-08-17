c18eb22a90264410c366c69d6d9e0f6912faaaec
Syed.Reshma Ruksana <reshmasmile468@achiever-107.local>
2019-08-17 10:26:48 +0530

    Initial Commit

---
+++ TENTH AND INTER USING STRUCTS 2Tests/TENTH_AND_INTER_USING_STRUCTS_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TENTH_AND_INTER_USING_STRUCTS_2Tests.swift
+//  TENTH AND INTER USING STRUCTS 2Tests
+//
+//  Created by Syed.Reshma Ruksana on 8/17/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import XCTest
+@testable import TENTH_AND_INTER_USING_STRUCTS_2
+
+class TENTH_AND_INTER_USING_STRUCTS_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
